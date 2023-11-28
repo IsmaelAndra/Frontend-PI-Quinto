7db747428db8376098e4caa6d789f012b57c509c
IsmaelAndra <jic.andrade@yavirac.edu.ec>
2023-11-28 02:35:11 -0500

    Cambios 2 28/11/2023

--- src/app/modules/appointment/appointment-routing.module.ts
+++ src/app/modules/appointment/appointment-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppointmentHomeComponent } from './appointment-home/appointment-home.component';
-import { ScheduleAppointmentComponent } from './schedule-appointment/schedule-appointment.component';
 
 const routes: Routes = [
       {
@@ -10,7 +9,6 @@ const routes: Routes = [
           [
             {path: 'appointment', pathMatch: 'full', redirectTo: 'appointment-home'},
             { path: 'appointment-home', component: AppointmentHomeComponent },
-            { path: 'schedule-appointment', component: ScheduleAppointmentComponent },
           ]
       }
 ];
--- src/app/modules/appointment/appointment.module.ts
+++ src/app/modules/appointment/appointment.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppointmentHomeComponent } from './appointment-home/appointment-home.component';
 import { AppointmentRoutingModule } from './appointment-routing.module';
-import { ScheduleAppointmentComponent } from './schedule-appointment/schedule-appointment.component';
 
 @NgModule({
   declarations: [
-    AppointmentHomeComponent,
-    ScheduleAppointmentComponent
+    AppointmentHomeComponent
   ],
   imports: [
     CommonModule,
