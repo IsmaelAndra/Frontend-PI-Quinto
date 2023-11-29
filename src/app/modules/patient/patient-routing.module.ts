7a0daab0cf1050e9c99415c5aee024bc1b5a9134
IsmaelAndra <jic.andrade@yavirac.edu.ec>
2023-11-28 21:43:08 -0500

    Cambios 3 28/11/2023

--- src/app/modules/patient/patient-routing.module.ts
+++ src/app/modules/patient/patient-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { PatientHomeComponent } from './patient-home/patient-home.component';
 
 
@@ -9,8 +9,8 @@ const routes: Routes = [
     path: '',
     children:
       [
-        {path: 'patient-home', component:PatientHomeComponent},
         { path: '', pathMatch: 'full', redirectTo: 'patient' },
+        { path: 'patient-home', component: PatientHomeComponent },
       ]
   }
 
@@ -18,9 +18,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class PatientRoutingModule { }
